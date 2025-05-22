5d1a1fb65c99e24faf3e063e58f4516491c609e4
infoshark <101701552+Hashbury1@users.noreply.github.com>
2025-05-22 12:12:20 +0100

    Update my contract.sol

--- contract-8b09d5fad5.sol
+++ contract-8b09d5fad5.sol
@@ -2,54 +2,14 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.27;
 
-import {AccessControl} from "@openzeppelin/contracts@5.3.0/access/AccessControl.sol";
-import {ERC20} from "@openzeppelin/contracts@5.3.0/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @custom:security-contact infoshark01@gmail.com
+/// @custom:security-contact infoshark01@gmail.com for bugs
 contract Hashy is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor(address defaultAdmin, address minter) ERC20("hashy", "// SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.27;
-
-import {AccessControl} from "@openzeppelin/contracts@5.3.0/access/AccessControl.sol";
-import {ERC20} from "@openzeppelin/contracts@5.3.0/token/ERC20/ERC20.sol";
-
-/// @custom:security-contact infoshark01@gmail.com
-contract Hashy is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
-    constructor(address defaultAdmin, address minter) ERC20("hashy", "// SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.27;
-
-import {AccessControl} from "@openzeppelin/contracts@5.3.0/access/AccessControl.sol";
-import {ERC20} from "@openzeppelin/contracts@5.3.0/token/ERC20/ERC20.sol";
-
-/// @custom:security-contact infoshark01@gmail.com
-contract Hashy is ERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
-    constructor(address defaultAdmin, address minter) ERC20("0x608dE821e5110d915659b337b8D5307544e6dae7", "0x608dE821e5110d915659b337b8D5307544e6dae7") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
-}
-") {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
-}
-") {
+    constructor(address defaultAdmin, address minter) ERC20("hashy", "HSY") {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
         _grantRole(MINTER_ROLE, minter);
     }
